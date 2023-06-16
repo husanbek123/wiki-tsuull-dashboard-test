4cc479b8ac5641050395e3a191cc6b48cd72f881
Xasanjon <xasanjonabdunabiyev222@gmail.com>
2023-06-16 11:33:08 +0500

    Я добавляю Language DropDown и тему

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,33 @@
-import style from "./sidebar.module.scss";
+import * as React from "react";
 import { listItems } from "../../utils/routes/listItems";
+import { CaretDownOutlined, GlobalOutlined } from "@ant-design/icons";
+import { LanguageItemsProps } from "../../types/defaultType";
+import UzFlag from "../../images/icons/flag-uzbekistan.webp";
+import EnFlag from "../../images/icons/enFlag.webp";
+import style from "./sidebar.module.scss";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import { useTheme } from "../../utils/zustand/useTheme";
+const items: LanguageItemsProps[] = [
+  {
+    key: "1",
+    label: "Uzbek",
+    img: UzFlag,
+  },
+  {
+    key: "2",
+    label: "English",
+    img: EnFlag,
+  },
+];
+
 export default function Sidebar() {
+  const theme = useTheme((state) => state.theme);
+  const changeThemeFunction = useTheme((state) => state.changeTheme);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <header className={style.sidebar}>
       <nav className={`${style.sidebarNav}`}>
@@ -22,7 +49,49 @@ export default function Sidebar() {
             </li>
           ))}
         </ul>
-        <div className="lang-container">
+        <div className={style.Language_themeContainer}>
+          <div className="lang">
+            <div className={style.Dropdown}>
+              <div className={style.DropdownWrapper} onClick={handleOpen}>
+                <article className={style.DropdownControl}>
+                  <GlobalOutlined className={style.DropdownBtn} />
+                  <span>Language</span>
+                </article>
+                <article>
+                  <CaretDownOutlined />
+                </article>
+              </div>
+              {open ? (
+                <div className={style.DropdownList}>
+                  {items?.map(({ img, key, label }) => (
+                    <div className={style.DropdownItem} key={key}>
+                      <div className="drob-img">
+                        <img
+                          src={img}
+                          width={70}
+                          height={70}
+                          className={style.drobImg}
+                          alt={label}
+                        />
+                      </div>
+                      <span>{label}</span>
+                    </div>
+                  ))}
+                </div>
+              ) : null}
+            </div>
+          </div>
+          <div className={style.theme}>
+            {theme === "light" ? (
+              <div  onClick={() => changeThemeFunction() } className={style.lightTheme}>
+                <BsFillSunFill /> 
+              </div>
+            ) : (
+              <div onClick={() => changeThemeFunction()} className={style.darkTheme}>
+                <BsFillMoonFill /> 
+              </div>
+            )}
+          </div>
         </div>
       </nav>
     </header>
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 import { ChildrenType } from "../types/defaultType";
 import Sidebar from "../components/Sidebar/Sidebar";
 import style from "./layout.module.scss"
+import { useTheme } from "../utils/zustand/useTheme";
 export default function Layout(props: ChildrenType): React.ReactElement {
+  const theme = useTheme(state => state.theme);
   return (
-    <section className={style.layoutWrapper}>
+    <section id={theme} className={style.layoutWrapper}>
       <Sidebar />
       {props?.children}
     </section>
