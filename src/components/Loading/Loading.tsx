76df9cddd75e15699709cf13a1958e18dc300553
Nurjahon Ergashev <lobarqochqorova18@gmail.com>
2023-06-24 15:10:10 +0500

    loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,7 +3,6 @@ export default function Loading(){
   return (
     <main className={style.loaderWrapper}> 
         <span className={style.loader}></span>
-        <h4 className={style.loadingTitle}>Loading...</h4>
     </main>
   )
 }
