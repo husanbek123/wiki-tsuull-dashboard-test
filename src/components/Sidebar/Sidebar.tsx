33b8cceea62c85f9aafa01f01188a87db7ae50cf
Xasanjon <xasanjonabdunabiyev222@gmail.com>
2023-06-24 18:41:08 +0500

    last commit

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,94 +1,63 @@
-import { changeLanguage } from "i18next";
+import { listItemType } from "../../types/defaultType";
 import { listItems } from "../../utils/routes/listItems";
-import style from "./sidebar.module.scss";
-import { useTheme } from "../../utils/zustand/useTheme";
-import { NavLink, useNavigate } from "react-router-dom";
-import { Button, Dropdown } from "antd";
-import { useLanguage } from "../../utils/zustand/useLanguage";
-import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
-import { t } from "i18next";
-import { useToken } from "../../utils/zustand/useStore";
-import uzImg from "../../images/icons/flag-uzbekistan.webp";
-import enImg from "../../images/icons/enFlag.webp";
-import { MdLanguage } from "react-icons/md";
-const items = [
-  {
-    key: "uz",
-    label: (
-      <img style={{ margin: "10px 0" }} src={uzImg} width={70} alt="uz-img" />
-    ),
-  },
-  {
-    key: "en",
-    label: <img src={enImg} width={70} alt="en-img" />,
-  },
-];
-export default function Sidebar() {
-  const setTheme = useTheme((state) => state.setTheme);
-  const setLanguage = useLanguage((state) => state.setLangauge);
-  const setToken = useToken((state) => state.setToken);
-  const theme = useTheme((state) => state.theme);
-  const language = useLanguage((state) => state.langauge);
-  const name = useToken((state) => state.name);
-  const router = useNavigate();
-  changeLanguage(language);
+import React from "react";
+// BootstrapIcons
+import { BsArrowLeftShort } from "react-icons/bs";
+import { FiBookOpen } from "react-icons/fi";
+// List Items
+// RiDashbord Icon
+import { RiDashboardFill } from "react-icons/ri";
+// listItamsType
+type Props = {};
+function Sidebar({}: Props) {
+  // Open State
+  const [open, setOpen] = React.useState<boolean>(true);
+  // Toggle Function for Change Open State
+  const toggle = () => setOpen((prew) => !prew);
   return (
-    <header className={style.header}>
-      <nav className={style.navbar}>
-        <div className={style.userMe}>
-          <div className={style.ProfilePhoto}>{name?.[0]}</div>
-          <div className={style.userTitles}>
-            <h2>{name}</h2>
-            <p>admin</p>
-          </div>
-        </div>
-        <div className={style.links}>
-          {listItems.map((item, index) => (
-            <NavLink to={`${item.url}`} key={index}>
-              {t(item.title)}
-            </NavLink>
-          ))}
-        </div>
-        <div className={style.params}>
-          <div className={style.languages}>
-            <Dropdown
-              menu={{
-                items,
-                selectable: true,
-                defaultValue: language,
-                defaultSelectedKeys: [language],
-                onSelect: (e) => setLanguage(e.key),
-              }}
-              placement="bottom"
-              arrow={{ pointAtCenter: true }}
+    <div
+      className={`bg-dark-purple h-screen  duration-300 p-5 pt-8 ${
+        open ? "w-72" : "w-60"
+      } relative`}
+    >
+      {/* Logo */}
+      <div id="p-logo" className="flex my-3 items-center gap-5" role="banner">
+        <span>
+          <FiBookOpen className={`text-white text-[30px] ${!open && "text-[40px]"}`} />
+        </span>
+        <a
+          className={`text-white text-[26px] duration-200 ${
+            !open && "scale-0"
+          }`}
+          href="/"
+          title="Visit the main page"
+        >
+         Edu Finder
+        </a>
+      </div>
+      {/* Change  Icon*/}
+      <BsArrowLeftShort
+        onClick={toggle}
+        className={`bg-white text-dark-purple text-3xl rounded-full absolute border-2  cursor-pointer  border-dark-purple -right-3 top-9 ${
+          !open && "rotate-180"
+        }`}
+      />
+
+      {listItems?.map((item: listItemType, id) => {
+        return (
+          <ul key={id} className="pt-4">
+            <li
+              className="cursor-pointer p-2 flex items-center gap-3 text-gray-300 rounded duration-300 hover:bg-light-white"
+              onClick={() => (window.location.pathname = item?.url)}
             >
-              <MdLanguage size={40} />
-            </Dropdown>
-          </div>
-          <div
-            className={style.theme}
-            onClick={()=>setTheme(theme == "dark" ? "light" : "dark")}
-          >
-            {theme === "light" ? (
-              <div className={style.ThemContainerSun}>
-                <BsFillSunFill className={style.SunIcon} />
-              </div>
-            ) : (
-              <div className={style.ThemeContainerMoon}>
-                <BsFillMoonFill className={style.MoonIcon} />
-              </div>
-            )}
-          </div>
-          <Button
-            onClick={() => {
-              setToken("");
-              router("/login");
-            }}
-          >
-            {t("Log out")}
-          </Button>
-        </div>
-      </nav>
-    </header>
+              <RiDashboardFill className="text-2xl" />
+              <p> {item?.title}</p>
+            </li>
+          </ul>
+        );
+      })}
+    </div>
   );
 }
+
+export default Sidebar;
