5039fd3f18a787d8eaac5b64ef082cafcb6d1482
Nurjahon Ergashev <lobarqochqorova18@gmail.com>
2023-06-25 13:45:50 +0500

    breadcrumb styles

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,17 +1,25 @@
 import { Breadcrumb } from "antd";
 export const ComponenBreadCrumb = (props: { url: string }) => {
   return (
-    <Breadcrumb
-      items={[
-        {
-          title: <b style={{
-            fontSize: "17px",
-            backgroundColor : "lightgray",
-            // paddingBottom: "5px",
-            padding: "2px 20px"
-          }}>{props.url}</b>,
-        },
-      ]}
-    />
+    <div className="bredcrumb">
+      <Breadcrumb
+        items={[
+          {
+            title: (
+              <b
+                style={{
+                  fontSize: "22px",
+                  padding: "2px 20px",
+                  width: "100%",
+                  textAlign: "center",
+                }}
+              >
+                {props.url}
+              </b>
+            ),
+          },
+        ]}
+      />
+    </div>
   );
 };
